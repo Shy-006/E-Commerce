d1addd61306a6af82604e2c749a612bc955f8a4e
Shy-006 <shyamnathab2006@gmail.com>
2025-12-22 21:34:04 +0530

    project completed

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,44 +1,41 @@
-import { decode } from "jsonwebtoken";
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 
-export const protectRoute = async (req,res,next)=>{
-    
-    try{
-        const accesstoken = req.cookies.accesstoken;
+export const protectRoute = async (req, res, next) => {
+	try {
+		const accessToken = req.cookies.accessToken;
 
-        if(!accesstoken){
-            res.status(401).json({message:"Unauthorised - No accesstoke provided"});
-        }
-       try {
+		if (!accessToken) {
+			return res.status(401).json({ message: "Unauthorized - No access token provided" });
+		}
 
-         const decoded = jwt.verify(accesstoken,process.env.ACCESS_TOKEN_SECRET);
-        const user = await User.findById(decoded.userid).select("-password");
+		try {
+			const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+			const user = await User.findById(decoded.userId).select("-password");
 
-         if(!user){
-            res.status(401).json({message:"User not found"});
-        }
-        req.user = user;
-        next();
-        
-       } catch (err) {
-        if(err.name ==="TokenExpiredError"){
-             res.status(401).json({message:"Unauthorised -  accesstoken expired"});
-        }
-        throw err;
-        
-       }
-    }
-    catch(err){
-        res.status(401).json({message:"unauthorised -Invaild access token"});
-    }
-}
+			if (!user) {
+				return res.status(401).json({ message: "User not found" });
+			}
 
-export const adminRoute = (req,res,next)=>{
-    if(req.user && req.user.role === "admin"){
-        next();
-    }
-    else{
-        return res.status(403).json({message:"Access Denied - admin only"});
-    }
-}
+			req.user = user;
+
+			next();
+		} catch (error) {
+			if (error.name === "TokenExpiredError") {
+				return res.status(401).json({ message: "Unauthorized - Access token expired" });
+			}
+			throw error;
+		}
+	} catch (error) {
+		console.log("Error in protectRoute middleware", error.message);
+		return res.status(401).json({ message: "Unauthorized - Invalid access token" });
+	}
+};
+
+export const adminRoute = (req, res, next) => {
+	if (req.user && req.user.role === "admin") {
+		next();
+	} else {
+		return res.status(403).json({ message: "Access denied - Admin only" });
+	}
+};
---
+++ frontend/src/lib/stripe.js
@@ -0,0 +1,5 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+export const stripePromise = loadStripe(
+  "pk_test_51SeyovJEr5wNjZC16GcAzc3BZKrR8RCyQyIKarJCbxrL0VxaGjbqMfyUiTAziFjh135m7RqBuVz6nMoeXVq9I4pj00IHvZrDIT"
+);
