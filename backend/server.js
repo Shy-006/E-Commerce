86f8c4905caaeb7be87180f9d6ba5197e7539af5
Shy-006 <shyamnathab2006@gmail.com>
2025-12-22 22:36:26 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
@@ -29,9 +30,11 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
+  
   app.get(/.*/, (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "frontend", "dist", "index.html")
@@ -39,3 +42,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
