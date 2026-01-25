4bcdb37b2ca1058469f19abaf4da4c42766ebdcb
Sanjay <josephstudent001@gmail.com>
2026-01-25 14:32:28 +0530

    Distribution completed

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const __dirname = path.resolve();
 // ✅ CORS FIRST
 app.use(
   cors({
-    origin: ["http://localhost:8080", "http://localhost:5173"],
+    origin: [process.env.CLIENT_URL],
     credentials: true,
   })
 );
