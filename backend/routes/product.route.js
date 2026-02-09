50004e1398abda3c2b9b48987c3dc0536d4f42e2
Shyam <shyamnathab2006@gmail.com>
2026-02-09 22:52:28 +0530

    Reset repository (clean slate)

--- backend/routes/product.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Router } from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
-import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-
-const router = Router();
-
-router.get("/",protectRoute,adminRoute,getAllProducts);
-export default router;
