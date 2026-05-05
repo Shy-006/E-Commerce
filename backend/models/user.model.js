51ed21f33bd39f421ade23eca12aa7779da6bd85
Shyamnath A B <164893418+Shy-006@users.noreply.github.com>
2026-05-05 13:13:23 +0530

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -64,7 +64,8 @@ userSchema.pre("save", async function () {
   
   if (!this.password) return;
 
-  this.password = await bcrypt.hash(this.password, 10);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 
